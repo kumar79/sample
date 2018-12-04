f9ad3cc85e521024f4acf540ea1c63488f78e680
Kumarasamy <kumarky7@gmail.com>
2018-12-04 22:37:05 +0530

    Fourth commit

---
+++ src/main/java/com/apps/classes/Brains.java
@@ -0,0 +1,15 @@
+package com.apps.classes;
+
+public class Brains {
+	float intracranialVolume;
+
+	public float getIntracranialVolume() {
+		return intracranialVolume;
+	}
+
+	public void setIntracranialVolume(float intracranialVolume) {
+		this.intracranialVolume = intracranialVolume;
+	}
+	
+	
+}
